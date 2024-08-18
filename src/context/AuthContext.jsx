3695dcf973f950a2ffb4a8df097b50dfb4569d23
MelvinYG <melvingeorge204@gmail.com>
2024-08-18 08:21:02 +0530

    feat: axios and login handler added

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,28 +1,22 @@
 import { createContext, useEffect, useState } from "react";
 
-// Create AuthContext
 export const AuthContext = createContext();
 
-// AuthContextProvider Component
 export const AuthContextProvider = ({ children }) => {
-    // Initialize currentUser state from localStorage or null
     const [currentUser, setCurrentUser] = useState(() => {
-        // Use lazy initialization to ensure it runs only once on mount
         const storedUser = localStorage.getItem("user");
         return storedUser ? JSON.parse(storedUser) : null;
     });
 
-    // Update user function
     const updateUser = (data) => {
         setCurrentUser(data);
     };
 
-    // Sync currentUser with localStorage whenever it changes
     useEffect(() => {
         if (currentUser) {
             localStorage.setItem("user", JSON.stringify(currentUser));
         } else {
-            localStorage.removeItem("user");  // If user is null, clean up storage
+            localStorage.removeItem("user");  
         }
     }, [currentUser]);
 
--- src/routes/login/login.jsx
+++ src/routes/login/login.jsx
@@ -1,14 +1,41 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './login.css';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import apiRequest from '../../lib/apiRequest';
 
 const Login = () => {
+
+  const [error, setError] = useState("");
+  const {updateUser} = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const loginHandler = async (event) => {
+    event.preventDefault();
+    setError("");
+    const formData = new FormData(event.target);
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try{
+      const res = await apiRequest.post("/auth/login", {
+        email,password
+      });
+      updateUser(res.data);
+      navigate('/chats');
+    }catch(err){
+      setError(err.response.data.message);
+    }
+  } 
+
   return (
     <div className='login-container h-[100vh] flex flex-col justify-center items-center'>
       <div className='flex flex-col justify-center items-center'>
-        <form className='flex flex-col gap-4'>
+        <form className='flex flex-col gap-4' onSubmit={loginHandler}>
             <h2 className='text-center font-semibold'>Welcome Back</h2>
             <input type="email" name="email" id="email" placeholder="Email" required/>
             <input type="password" name="password" id="password" placeholder="Password" required/>
+            {error && <span>{error}</span>}
             <button className='bg-orange-300 border border-orange-800 p-2 hover:bg-orange-700 transition-all ease-in-out'>
                 Login
             </button>
